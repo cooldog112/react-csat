b46ff11ccdb3ce323958ad6c4a2bbff01628edc2
cooldog112 <cooldog112@gmail.com>
2020-12-10 12:25:25 +0900

    Add Node.js Express Server Module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app =express();
@@ -6,7 +7,49 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extends:true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
 });
+connection.connect();
+
+const multer = require('multer');
+const upload = multer({dest : './upload'})
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+          res.send(rows);
+        }
+    );
+});
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) =>{
+   let sql =  'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+   let image = '/image/' + req.file.filename;
+   let name = req.body.name;
+   let birthday = req.body.birthday;
+   let gender = req.body.gender;
+   let job = req.body.job;
+
+   console.log(req.body.name);
+
+   let params = [image, name, birthday, gender, job];
+
+   connection.query(sql, params,
+       (err, rows, fields) =>{
+        res.send(rows);
+       })
+
+});
+
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
