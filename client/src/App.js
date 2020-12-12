25e235489a18db5cccba4eb540179b1671473f69
cooldog112 <cooldog112@gmail.com>
2020-12-12 12:56:37 +0900

    develop delete actions

--- client/src/App.js
+++ client/src/App.js
@@ -15,14 +15,14 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 const styles = theme => ({
   root: {
     width : '100%',
-    marginTop : theme.spacing.unit * 3,
+    marginTop : theme.spacing(1) * 3,
     overflowX : "auto"
   },
   table:{
     minWidth: 1080
   },
   progress:{
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(1) * 2
   }
 })
 
@@ -77,12 +77,13 @@ class App extends Component{
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {
                 this.state.customers ?
-                  this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday}  gender={c.gender} job={c.job} />);}):
+                  this.state.customers.map(c => { return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday}  gender={c.gender} job={c.job} />);}):
                   <TableRow>
                     <TableCell colSpan="6" align="center">
                       <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
